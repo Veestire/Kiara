5ac5dcaef3090f19acecc05bc335da5cd4bf5f8e
rooni <nekorooni@gmail.com>
2017-10-03 18:54:03 +0200

    Change 8ball formatting

--- cogs/Rng.py
+++ cogs/Rng.py
@@ -46,11 +46,13 @@ class Rng:
 
     @commands.command(aliases=['8', '8b', '8ball'])
     async def ball(self, ctx, *, question=None):
-        chance = ["It is certain","It is decidedly so","Without a doubt","Yes definitely","You may rely on it",
-                  "As I see it, yes","Most likely","Outlook good","Yes","Signs point to yes","Reply hazy try again",
-                  "Ask again later","Better not tell you now","Cannot predict now","Concentrate and ask again",
-                  "Don't count on it","My reply is no","My sources say no","Outlook not so good","Very doubtful"]
-        await ctx.send(random.choice(chance))
+        answers = [
+            "It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it",
+            "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again",
+            "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
+            "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"
+        ]
+        await ctx.send(random.choice(answers))
 
     @commands.command(aliases=['pick', 'choice'])
     async def choose(self, ctx, *choices):
