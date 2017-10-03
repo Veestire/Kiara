2b9e277281532465687a7e39493d7a372d6e772f
rooni <nekorooni@gmail.com>
2017-10-03 18:54:28 +0200

    Update Level cog

--- cogs/Level.py
+++ cogs/Level.py
@@ -10,6 +10,18 @@ class Gains(Enum):
     message = 10
     attachment = 5
 
+
+def exp_needed(level):
+    return 50 + round(5 * (level ** 2) - (5 * level))
+
+
+def total_exp(level):
+    i = 0
+    for l in range(1, level - 1):
+        i += exp_needed(l)
+    return i
+
+
 class Level:
     """A cog for checking activity in the form of experience"""
 
@@ -28,25 +40,19 @@ class Level:
             return
         l, x = self.data[msg.author.id]
         x += Gains.message.value
-        needed = self.exp_needed(l)
+        needed = exp_needed(l)
         if x >= needed:
             l += 1
             x -= needed
-            await self.ch.send(f'{msg.author} leveled up to {l}, new xp: {x}/{self.exp_needed(l)}')
+            await self.ch.send(f'{msg.author} leveled up to {l}, new xp: {x}/{exp_needed(l)}')
         self.data[msg.author.id] = l, x
 
     @commands.command()
     async def testxp(self, ctx, max: int, jump: int):
         output = ''
         for i in range(1, max, jump):
-            output += f'Lv {i} - {self.exp_needed(i)}xp needed for lv{i+1}\n'
+            output += f'Lv {i} - {exp_needed(i)}xp needed for lv{i+1}\n'
         await ctx.send(output)
-
-    def get_level(self, xp):
-        return math.sqrt(xp)
-
-    def exp_needed(self, level):
-        return 50+round(5 * (level ** 2) - (5 * level))
 
 
 def setup(bot):
