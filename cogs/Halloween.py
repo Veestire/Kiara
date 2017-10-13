6bacff56fc81ef3967ea05370b0792cea21199f8
rooni <nekorooni@gmail.com>
2017-10-13 17:47:29 +0200

    Fix permissions

--- cogs/Halloween.py
+++ cogs/Halloween.py
@@ -6,10 +6,18 @@ import asyncio
 import discord
 from discord.ext import commands
 
+
 def custom_format(td):
     minutes, seconds = divmod(td.seconds, 60)
     hours, minutes = divmod(minutes, 60)
     return '{:d}:{:02d}'.format(hours, minutes)
+
+
+def can_change():
+    async def predicate(ctx):
+        return ctx.author.id in [73389450113069056, 211238461682876416]
+    return commands.check(predicate)
+
 
 class Halloween:
     """Commands for the halloween event~"""
@@ -19,6 +27,7 @@ class Halloween:
         self.conf = {}
         with open('trickortreat.json') as file:
             self.conf = json.load(file)
+
 
     @commands.group(aliases=['tot'])
     @commands.guild_only()
@@ -52,6 +61,7 @@ class Halloween:
         await ctx.send(embed=emb)
 
     @trickortreat.command()
+    @can_change()
     async def chance(self, ctx, *, value: float = None):
         if value:
             self.conf['chance'] = value
@@ -62,6 +72,7 @@ class Halloween:
             await ctx.send(self.conf['chance'])
 
     @trickortreat.command()
+    @can_change()
     async def prize(self, ctx, *, value=None):
         if value:
             self.conf['prize'] = value
@@ -72,6 +83,7 @@ class Halloween:
             await ctx.send(self.conf['prize'])
 
     @trickortreat.command()
+    @can_change()
     async def cooldown(self, ctx, *, value: float = None):
         if value:
             self.conf['cooldown'] = value
