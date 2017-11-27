7cbe2d34817311ca79dee0b55ade616d7275b1f7
rooni <nekorooni@gmail.com>
2017-11-27 15:26:26 +0100

    Add moderation cog

---
+++ cogs/moderation.py
@@ -0,0 +1,98 @@
+from discord.ext import commands
+
+import asyncio
+import discord
+import random
+
+
+STAFF_CHANNEL = 231008480079642625
+
+class MemberID(commands.Converter):
+    async def convert(self, ctx, argument):
+        try:
+            m = await commands.MemberConverter().convert(ctx, argument)
+        except commands.BadArgument:
+            try:
+                return int(argument, base=10)
+            except ValueError:
+                raise commands.BadArgument(f"{argument} is not a valid member or member ID.") from None
+        else:
+            can_execute = ctx.author.id == ctx.bot.owner_id or \
+                          ctx.author == ctx.guild.owner or \
+                          ctx.author.top_role > m.top_role
+
+            if not can_execute:
+                raise commands.BadArgument('You cannot do this action on this user due to role hierarchy.')
+            return m.id
+
+class BannedMember(commands.Converter):
+    async def convert(self, ctx, argument):
+        ban_list = await ctx.guild.bans()
+        try:
+            member_id = int(argument, base=10)
+            entity = discord.utils.find(lambda u: u.user.id == member_id, ban_list)
+        except ValueError:
+            entity = discord.utils.find(lambda u: str(u.user) == argument, ban_list)
+
+        if entity is None:
+            raise commands.BadArgument("Not a valid previously-banned member.")
+        return entity
+
+class Moderation:
+    """Moderation commands"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.has_any_role('Staff', 'Mini-Mod')
+    async def kick(self, ctx, member: discord.Member, *, reason=None):
+        permissions = ctx.channel.permissions_for(ctx.author)
+        if getattr(permissions, 'kick_members', None):
+            try:
+                await ctx.guild.kick(member, reason=reason)
+                await ctx.send(f'Kicked {member}!')
+            except Exception as e:
+                await ctx.send(e)
+        else:
+            if discord.utils.get(ctx.author.roles, name="Mini-Mod"):
+                ch = self.bot.get_channel(STAFF_CHANNEL)
+                await ch.send(f"{ctx.author.mention} requests kicking {member.mention}.")
+                await ctx.send('Your kick request has been received.')
+
+    @commands.command()
+    @commands.has_any_role('Staff', 'Mini-Mod')
+    async def ban(self, ctx, member: MemberID, *, reason=None):
+        permissions = ctx.channel.permissions_for(ctx.author)
+        if getattr(permissions, 'ban_members', None):
+            try:
+                await ctx.guild.ban(discord.Object(id=member), reason=reason)
+                member = await self.bot.get_user_info(member)
+                await ctx.send(f'Banned {member}!')
+            except Exception as e:
+                await ctx.send(e)
+        else:
+            if discord.utils.get(ctx.author.roles, name="Mini-Mod"):
+                ch = self.bot.get_channel(STAFF_CHANNEL)
+                await ch.send(f"{ctx.author.mention} requests banning {member.mention}.")
+                await ctx.send('Your ban request has been received.')
+
+
+    @commands.command()
+    @commands.has_any_role('Staff', 'Mini-Mod')
+    async def unban(self, ctx, member: BannedMember, *, reason=None):
+        permissions = ctx.channel.permissions_for(ctx.author)
+        if getattr(permissions, 'ban_members', None):
+            try:
+                await ctx.guild.unban(member.user, reason=reason)
+                await ctx.send(f'Unbanned {member.user}!')
+            except Exception as e:
+                await ctx.send(e)
+        else:
+            if discord.utils.get(ctx.author.roles, name="Mini-Mod"):
+                ch = self.bot.get_channel(STAFF_CHANNEL)
+                await ch.send(f"{ctx.author.mention} requests unbanning {member.user.mention}.")
+                await ctx.send('Your uban request has been received.')
+
+def setup(bot):
+    bot.add_cog(Moderation(bot))
