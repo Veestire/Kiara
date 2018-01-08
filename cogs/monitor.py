2eab053bb9395a70b18be86eda9e0ee735923a19
rooni <nekorooni@gmail.com>
2018-01-08 10:33:52 +0100

    Update monitor.py

--- cogs/monitor.py
+++ cogs/monitor.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 
+MONITOR_CHANNEL = 399852942213251083
 
 class MemberID(commands.Converter):
     async def convert(self, ctx, argument):
@@ -14,27 +15,53 @@ class MemberID(commands.Converter):
         else:
             return m.id
 
-
 class Monitor:
 
     def __init__(self, bot):
         self.bot = bot
         self.logged_users = []
 
-    @commands.command()
+    @commands.group()
     @commands.has_any_role('Staff')
     async def monitor(self, ctx, member: MemberID):
-
+        if ctx.invoked_subcommand is not None:
+            return
         self.logged_users += [member]
-        await ctx.send('')
+        member = await self.bot.get_user_info(member)
+        await ctx.send(f'Now monitoring {member}.')
+
+    @monitor.command(name="list")
+    @commands.has_any_role('Staff')
+    async def monitor_list(self, ctx):
+        if self.logged_users:
+            await ctx.send('Users currently getting logged:\n'+'\n'.join([self.bot.get_user(u) or u for u in self.logged_users]))
+        else:
+            await ctx.send('No users are getting logged atm.')
 
     @commands.command()
     @commands.has_any_role('Staff')
     async def unmonitor(self, ctx, member: MemberID):
-        self.logged_users += [member]
+        self.logged_users.remove(member)
+        member = await self.bot.get_user_info(member)
+        await ctx.send(f'No longer monitoring {member}.')
 
     async def on_member_join(self, member):
         if member.id in self.logged_users:
+            ch = self.bot.get_channel(MONITOR_CHANNEL)
+            await ch.send(f'{member.mention} joined the server.')
+
+    async def on_member_remove(self, member):
+        if member.id in self.logged_users:
+            ch = self.bot.get_channel(MONITOR_CHANNEL)
+            await ch.send(f'{member.mention} is no longer on the server.')
+
+    async def on_message_delete(self, message):
+        if message.author.id in self.logged_users:
+            ch = self.bot.get_channel(MONITOR_CHANNEL)
+            await ch.send(f'Message deleted from {message.author.mention}')
+            await ch.send(f'```\n{message.content}```')
+            if message.attachments:
+                await ch.send('\n'.join([x.url for x in message.attachments]))
 
 
 def setup(bot):
