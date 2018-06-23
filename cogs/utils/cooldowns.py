478da6d84e19621e549ecf297c617700ded0209c
Rooni <nekorooni@gmail.com>
2018-06-24 01:08:52 +0200

    Update cooldowns
    Temporary

--- cogs/utils/cooldowns.py
+++ cogs/utils/cooldowns.py
@@ -3,5 +3,10 @@ from discord.ext import commands
 
 def basic_cooldown(seconds):
     async def predicate(ctx):
-        cd = await ctx.redis.ttl()
+        key = f"cooldown:{ctx.author.id}:{ctx.command.qualified_name}"
+        cooldown = await ctx.redis.ttl(key)
+        if cooldown > 0:
+            raise commands.CommandOnCooldown(None, cooldown)
+        await ctx.redis.set(key, 1, expire=seconds)
+        return True
     return commands.check(predicate)
