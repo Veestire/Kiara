c6a5b1ef6f1177b5de0ef5c0fc8b9dd529bd52a6
Rooni <nekorooni@gmail.com>
2018-06-23 22:32:52 +0200

    Update context

--- cogs/utils/context.py
+++ cogs/utils/context.py
@@ -4,12 +4,11 @@ from discord.ext import commands
 class Context(commands.Context):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.redis = self.bot.redis
 
     @property
     def session(self):
         return self.bot.session
 
     @property
-    def rediss(self):
+    def redis(self):
         return self.bot.redis
