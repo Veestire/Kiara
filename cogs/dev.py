5f114fcc6fb3eddf515cf5cea401627e1a499957
Rooni <nekorooni@gmail.com>
2018-06-24 16:51:00 +0200

    Add dev restart command

--- cogs/dev.py
+++ cogs/dev.py
@@ -1,11 +1,22 @@
 import re
 
+from discord.ext import commands
+
 
 class Dev:
 
     def __init__(self, bot):
         self.bot = bot
         self.issue = re.compile(r'##(?P<number>[0-9]+)')
+
+    async def __local_check(self, ctx):
+        return await self.bot.is_owner(ctx.author) or ctx.author.id == 211238461682876416
+
+    async def on_ready(self):
+        ch = await self.bot.redis.get('restartmessage')
+        if ch:
+            ch = self.bot.get_channel(int(ch))
+            await ch.send("Back")
 
     async def on_message(self, message):
         if not message.guild:
@@ -16,6 +27,11 @@ class Dev:
             url = 'https://github.com/Nekorooni/Kiara/issues/'
             await message.channel.send(url + m.group('number'))
 
+    @commands.command(hidden=True)
+    async def restart(self, ctx):
+        await ctx.redis.set('restartmessage', ctx.channel.id)
+        await ctx.send("Restarting")
+        exit(1)
 
 def setup(bot):
     bot.add_cog(Dev(bot))
