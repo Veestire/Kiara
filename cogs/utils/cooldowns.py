9ef0470b11bbb759e5fc0b5db4cad98aeeded09d
Rooni <nekorooni@gmail.com>
2018-06-24 20:03:11 +0200

    Update cooldown

--- cogs/utils/cooldowns.py
+++ cogs/utils/cooldowns.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 
 def basic_cooldown(seconds):
     async def predicate(ctx):
+        if ctx.invoked_with.lower() == 'help':
+            return True
         key = f"cooldown:{ctx.author.id}:{ctx.command.qualified_name}"
         cooldown = await ctx.redis.ttl(key)
         if cooldown > 0:
