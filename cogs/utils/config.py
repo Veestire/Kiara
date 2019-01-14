1e02b40cf9d6ed44fb18c85b112c7d3e25aa7ebe
Rooni <nekorooni@gmail.com>
2019-01-14 01:26:48 +0100

    Update config class to try converting to int

--- cogs/utils/config.py
+++ cogs/utils/config.py
@@ -8,5 +8,8 @@ class Config:
     def __getattr__(self, name):
         name = name.upper()
         if name not in self._data:
-            self._data[name] = os.getenv(name)
+            try:
+                self._data[name] = int(os.getenv(name))
+            except ValueError:
+                self._data[name] = os.getenv(name)
         return self._data[name]
